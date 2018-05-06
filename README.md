40b7132b3fa1d32b25a7ef72bac98a085c071af1
Jorge Matricali <jorgematricali@gmail.com>
2018-05-05 22:26:57 -0300

    + Base application

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # phalcon-press
+
+[![Build Status](https://travis-ci.org/jorge-matricali/phalcon-press.svg?branch=master)](https://travis-ci.org/jorge-matricali/phalcon-press) [![license](https://img.shields.io/github/license/jorge-matricali/phalcon-press.svg)](LICENSE.txt) [![GitHub contributors](https://img.shields.io/github/contributors/jorge-matricali/phalcon-press.svg)](https://github.com/jorge-matricali/phalcon-press/graphs/contributors)
---
+++ app/config/config.php
@@ -0,0 +1,53 @@
+<?php
+/*
+
+Copyright (C) 2018 Jorge Matricali
+
+This file is part of Phalcon-Press.
+
+Phalcon-Press is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Phalcon-Press is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Phalcon-Press.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
+ * NOTE: please remove this comment.
+ */
+defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
+defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
+
+return new \Phalcon\Config([
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => '',
+        'dbname'      => 'test',
+        'charset'     => 'utf8',
+    ],
+    'application' => [
+        'appDir'         => APP_PATH . '/',
+        'controllersDir' => APP_PATH . '/controllers/',
+        'modelsDir'      => APP_PATH . '/models/',
+        'migrationsDir'  => APP_PATH . '/migrations/',
+        'viewsDir'       => APP_PATH . '/views/',
+        'pluginsDir'     => APP_PATH . '/plugins/',
+        'libraryDir'     => APP_PATH . '/library/',
+        'cacheDir'       => BASE_PATH . '/cache/',
+
+        // This allows the baseUri to be understand project paths that are not in the root directory
+        // of the webpspace.  This will break if the public/index.php entry point is moved or
+        // possibly if the web server rewrite rules are changed. This can also be set to a static path.
+        'baseUri'        => preg_replace('/public([\/\\\\])index.php$/', '', $_SERVER['PHP_SELF']),
+    ]
+]);
